baa7617668f1cbb4652652517a0f013720a3beb8
iceiceice <84047808+IndianBleach@users.noreply.github.com>
2024-01-10 17:54:43 +0300

    malloc: add alloced[] shift_left

--- malloc/foo.c
+++ malloc/foo.c
@@ -106,12 +106,25 @@ int _indexOfMinFreed(size_t target, int startIndex, int endIndex) {
 
     int mid = (endIndex - startIndex) / 2;
 
+    int prev = -1;
+
     while(startIndex <= endIndex) {
 
         mid = (endIndex - startIndex) / 2;
 
+        if (prev == mid ||
+            prev - mid == -1) {
+                mid = prev;
+                break;
+            }
+
+        prev = mid;
+
+        printf("mid=%i\n", mid);
+
         if (heap.alloced_chunks[mid].size > target) {
             endIndex = mid--;
+            //
         }
         else if (heap.alloced_chunks[mid].size < target) {
             startIndex = mid++;
@@ -129,7 +142,7 @@ int _indexOfMinFreed(size_t target, int startIndex, int endIndex) {
     if (mid <= 1) mid = 0;
 
     while(mid < heap.alloced_index) {
-        //printf("arr[%i] free=%i size=%i \n", mid, heap.alloced_chunks[mid].is_freed, heap.alloced_chunks[mid].size);
+        printf("arr[%i] free=%i size=%i \n", mid, heap.alloced_chunks[mid].is_freed, heap.alloced_chunks[mid].size);
         if (heap.alloced_chunks[mid].is_freed &&
             heap.alloced_chunks[mid].size >= target) {
             return mid;
@@ -139,6 +152,7 @@ int _indexOfMinFreed(size_t target, int startIndex, int endIndex) {
 
     return -1;
 }
+
 
 void dump_heap() {
     printf("dump: MHEAP\nalloced_cap+index: %i %i\nstart_at: %p\ncurr: %p\n----------------------\n", 
@@ -170,6 +184,91 @@ void dump_heap_array() {
 }
 
 
+void heap_shift_right(int startIndex) {
+    // todo: asserts
+    if (startIndex < ALLOCED_CHUNKS_LEN) {
+        
+        for(int i = heap.alloced_index-1; i >= startIndex; i--) {
+            heap.alloced_chunks[i+1] = heap.alloced_chunks[i];
+        }
+
+        heap.alloced_chunks[startIndex].size = 0; 
+
+        heap.alloced_index += 1;
+    }
+}
+
+void heap_shift_left(int startIndex, int toStep) {
+
+    if (startIndex < 0) return;
+
+    if (startIndex == heap.alloced_index - 1) startIndex = 0;
+
+    printf("================shift left==============\n");
+    dump_heap_array();
+    for(int i = startIndex; i < heap.alloced_index-toStep; i++) {
+        heap.alloced_chunks[i] = heap.alloced_chunks[i+toStep];
+    }
+
+    for(int i = heap.alloced_index-toStep; i < heap.alloced_index; i++) {
+        heap.alloced_chunks[i].size = 0;
+        heap.alloced_chunks[i].start_at = NULL;
+        heap.alloced_chunks[i].is_freed = false;
+        heap.alloced_index--;
+    }
+
+    dump_heap_array();
+    printf("================end==============\n");
+}
+
+// инсерт в чанк, разделяет его на две новые части
+void* insert_at(int index, size_t target_size) {
+    // update cur
+    // shift
+
+    if (index >= heap.alloced_index) {
+        return NULL;
+    }
+
+    size_t free_size = heap.alloced_chunks[index].size - target_size;
+    int new_index = index + 1;
+
+    //printf("st=%p\n", heap.alloced_chunks[index].start_at);
+    void* new_addr = heap.alloced_chunks[index].start_at + (int)target_size;
+
+    //printf(" size=%i\nleft: %p\nright:%p\n", target_size, heap.alloced_chunks[index].start_at, new_addr);
+
+    // update cur chunk 
+    heap.alloced_chunks[index].size = target_size;
+    heap.alloced_chunks[index].is_freed = false;
+
+    dump_heap_array();
+
+    if (free_size > 0) {
+        heap_shift_right(new_index);
+        HeapChunk right_chunk = {.is_freed = true, .size = free_size, .start_at = new_addr};
+        heap.alloced_chunks[new_index] = right_chunk;
+    }
+
+    dump_heap_array();
+
+    return new_addr;
+
+    // create new chunk
+
+    //heap.alloced_index++;
+    // todo: инкремент alloced_index to func
+}
+
+int sort_chunk_compare(const void* a, const void* b) {
+    HeapChunk ca = * ((HeapChunk*)a);
+    HeapChunk cb = * ((HeapChunk*)b);
+
+    if (ca.size == cb.size) return 0;
+    else if (cb.size > ca.size) return -1;
+    else return 1; 
+
+}
 
 void* _malloc(size_t size) {
     
@@ -179,9 +278,12 @@ void* _malloc(size_t size) {
 
     int freed_index = _indexOfMinFreed(size, 0, heap.alloced_index);
 
+    printf("(%i) f-index: %i \n", size, freed_index);
+
     if (freed_index != -1) {
+        insert_at(freed_index, size);
+        qsort(heap.alloced_chunks, heap.alloced_index, sizeof(HeapChunk), sort_chunk_compare);
         // insert_at
-        // - shift_on
         // - reorder
     }
     else {
@@ -191,6 +293,15 @@ void* _malloc(size_t size) {
 }
 
 // todo: попробовать реализовать фри через хеш таблицу
+
+// heap_collect
+// - update_cur
+// - shift_left
+
+// heap_sort
+
+
+
 
 
 void _free(void* addr) {
@@ -209,64 +320,9 @@ void _free(void* addr) {
 
 
 // склеивает все непрерывные чанки в один
-void heap_collect() {
 
-}
 
-void heap_reorder() {
-    
-}
 
-void heap_shift_right(int startIndex) {
-    // todo: asserts
-    if (startIndex < ALLOCED_CHUNKS_LEN) {
-        
-        for(int i = heap.alloced_index-1; i >= startIndex; i--) {
-            heap.alloced_chunks[i+1] = heap.alloced_chunks[i];
-        }
-
-        heap.alloced_chunks[startIndex].size = 0; 
-
-        heap.alloced_index += 1;
-    }
-}
-
-// инсерт в чанк, разделяет его на две новые части
-void* insert_at(int index, size_t target_size) {
-    // update cur
-    // shift
-
-    if (index >= heap.alloced_index) {
-        return NULL;
-    }
-
-    size_t free_size = heap.alloced_chunks[index].size - target_size;
-    int new_index = index + 1;
-
-    printf("st=%p\n", heap.alloced_chunks[index].start_at);
-    void* new_addr = heap.alloced_chunks[index].start_at + (int)target_size;
-
-    printf(" size=%i\nleft: %p\nright:%p\n", target_size, heap.alloced_chunks[index].start_at, new_addr);
-
-    // update cur chunk 
-    heap.alloced_chunks[index].size = target_size;
-    heap.alloced_chunks[index].is_freed = false;
-
-    dump_heap_array();
-
-    if (free_size > 0) {
-        heap_shift_right(new_index);
-        HeapChunk right_chunk = {.is_freed = true, .size = free_size, .start_at = new_addr};
-        heap.alloced_chunks[new_index] = right_chunk;
-    }
-
-    dump_heap_array();
-
-    // create new chunk
-
-    //heap.alloced_index++;
-    // todo: инкремент alloced_index to func
-}
 
 
 
@@ -282,11 +338,15 @@ void* insert_at(int index, size_t target_size) {
 // reorder
 
 
+
+
 int main() { 
 
     //_malloc(6);
    
     //dump_chunks();
+
+    
 
     int* p1 = (int*)_malloc(1);
 
@@ -294,24 +354,13 @@ int main() {
 
     int* p3 = (int*)_malloc(3);
 
-    _free(p3);
+    double* p4 = (double*)malloc(sizeof(double));
 
-    dump_heap_array();
+    heap_shift_left(1, 1);
 
-    // [1(0), 2(1), 3(0) ]
-    // insert_at(1, 1);
-    // [1(0), 1(0), 1(1), 3(0) ]
-
-    insert_at(2, 1);
-
-    dump_heap_array();
-
-    dump_chunks();
+    // 1 2 1 2(0)
 
     //printf("res=%i \n", _indexOfAddr((void*)p1, 0, heap.alloced_index));
-
-    //_indexOfAddr
-
 
     return 0;
 }
